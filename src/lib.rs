2645daad5538dda7a6e2172e37228c4fc82e1ed9
sam <contact@justsam.io>
2025-01-09 13:57:17 +0100

    chore: add simd behind feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,16 @@
-#![feature(portable_simd)]
-
 pub mod cache;
 pub mod fields;
 pub mod inline;
 pub mod traits;
 pub mod transform;
+#[cfg(feature = "simd")]
 pub mod simd;
+
+#[cfg(not(feature = "simd"))]
+mod simd {
+    // dummy module if SIMD is not enabled.
+    pub trait Vectorize {}
+}
 
 // Define optimal alignment for modern CPUs (ie. 32 bytes for AVX2)
 pub const ALIGNMENT: usize = 128;
